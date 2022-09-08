351c6f56519a1ce23151658a7d48b34e02009e84
michaeludoh <michaeludohgz@gmail.com>
2022-09-08 14:15:00 -0700

    program printing

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio>
+
+/**
+ * main - Printing
+ *
+ * Return: Always 0 (Successful)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
