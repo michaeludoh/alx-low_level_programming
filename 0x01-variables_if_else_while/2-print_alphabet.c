71f661d6b94bb22b1d8c65c73115fb55e42aaf0e
michaeludoh <michaeludohgz@gmail.com>
2022-09-09 14:27:58 -0700

    print alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,11 +8,12 @@
  */
 int main(void)
 {
-	char low;
+	int ch;
 
-	for (low = 'a'; low <= 'z'; low++)
-		put char(low);
-	printf("\n");
-
-	return(0);
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+return(0);
 }
