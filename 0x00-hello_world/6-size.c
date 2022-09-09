5634f794d144f5512deb72bc3ad6570467dc819f
michaeludoh <michaeludohgz@gmail.com>
2022-09-09 07:18:15 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -17,6 +17,6 @@ int main(void)
 	printf("Size of an int: %lu byte(s)\n", sizeof(b));
 	printf("Size of a long int: %lu byte(s)\n", sizeof(c));
 	printf("Size of a long long int: %lu byte(s)\n", sizeof(d));
-	printf("Size of a float: %lu bytes \n", sizeof(e));
+	printf("Size of a float: %lu bytes \n", sizeof(int));
 	return (0);
 }
