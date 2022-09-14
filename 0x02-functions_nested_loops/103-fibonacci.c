ca0a21e4a34170539ec320a4ec6842a11be7403b
michaeludoh <michaeludohgz@gmail.com>
2022-09-13 18:15:18 -0700

    fibonacci

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -6,7 +6,7 @@
  */
 int main(void)
 {
-	int a = 0; b = 1, next = 0;
+	int a = 0, b = 1, next = 0;
 	int sum = 0;
 
 	while (next < 4000000)
@@ -15,7 +15,7 @@ int main(void)
 		a = b;
 		b = next;
 		if (next % 2 == 0)
-			sum += next
+			sum += next;
 	}
 	printf("%i\n", sum);
 	return (0);
